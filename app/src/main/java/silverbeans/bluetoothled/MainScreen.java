c7aa35e5478dc4c4ab821c42696b2d147bb5366e
Tyler Tran <ttran@iastate.edu>
2015-04-14 13:17:46 -0500

    Fix

--- app/src/main/java/silverbeans/bluetoothled/MainScreen.java
+++ app/src/main/java/silverbeans/bluetoothled/MainScreen.java
@@ -34,11 +34,6 @@ public class MainScreen extends ActionBarActivity {
 
     Button btnRed, btnGreen, btnBlue, btnRGBBlink, btnMultiBlink, btnRandBlink, btnBrightBlink;
 
-    private static final int REQUEST_ENABLE_BT = 1;
-    private BluetoothAdapter btAdapter = null;
-    private BluetoothSocket btSocket = null;
-    private OutputStream outStream = null;
-
     // Well known SPP UUID
     private static final UUID MY_UUID =
             UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
@@ -46,10 +41,15 @@ public class MainScreen extends ActionBarActivity {
     // Insert your bluetooth devices MAC address
     private static String address = "00:06:66:6B:B6:A9";
 
+    BluetoothArduino talker = null;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        talker = new BluetoothArduino();
+
 
         Log.d(TAG, "In onCreate()");
         // btnRed, btnGreen, btnBlue, btnRGBBlink, btnMultiBlink, btnRandBlink, btnBrightBlink
@@ -67,12 +67,9 @@ public class MainScreen extends ActionBarActivity {
         LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
 
 
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        checkBTState();
-
         btnRed.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("0");
+                talker.SendMessage("0");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the Red LED", Toast.LENGTH_SHORT);
                 msg.show();
@@ -81,7 +78,7 @@ public class MainScreen extends ActionBarActivity {
 
         btnGreen.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("1");
+                talker.SendMessage("1");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the Green LED", Toast.LENGTH_SHORT);
                 msg.show();
@@ -89,7 +86,7 @@ public class MainScreen extends ActionBarActivity {
         });
         btnBlue.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("2");
+                talker.SendMessage("2");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the Blue LED", Toast.LENGTH_SHORT);
                 msg.show();
@@ -97,7 +94,7 @@ public class MainScreen extends ActionBarActivity {
         });
         btnRGBBlink.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("3");
+                talker.SendMessage("3");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the RGB Blinker", Toast.LENGTH_SHORT);
                 msg.show();
@@ -105,7 +102,7 @@ public class MainScreen extends ActionBarActivity {
         });
         btnMultiBlink.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("4");
+                talker.SendMessage("4");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the MultiColor Blinker", Toast.LENGTH_SHORT);
                 msg.show();
@@ -113,7 +110,7 @@ public class MainScreen extends ActionBarActivity {
         });
         btnRandBlink.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("5");
+                talker.SendMessage("5");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the Random Blinker", Toast.LENGTH_SHORT);
                 msg.show();
@@ -121,7 +118,7 @@ public class MainScreen extends ActionBarActivity {
         });
         btnBrightBlink.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                sendData("6");
+                talker.SendMessage("5");
                 Toast msg = Toast.makeText(getBaseContext(),
                         "You turned on the Bright LED Blinker", Toast.LENGTH_SHORT);
                 msg.show();
@@ -129,16 +126,18 @@ public class MainScreen extends ActionBarActivity {
         });
     }
 
-
-
     private BroadcastReceiver onNotice= new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            sendData("0");
             String pack = intent.getStringExtra("package");
             String title = intent.getStringExtra("title");
             String text = intent.getStringExtra("text");
+
+
+            if (pack.equalsIgnoreCase("com.groupme.android")){
+                talker.SendMessage("1");
+            }
 
 
             TableRow tr = new TableRow(getApplicationContext());
@@ -155,90 +154,6 @@ public class MainScreen extends ActionBarActivity {
         }
     };
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        Log.d(TAG, "...In onResume - Attempting client connect...");
-
-        // Set up a pointer to the remote node using it's address.
-        BluetoothDevice device = btAdapter.getRemoteDevice(address);
-
-        // Two things are needed to make a connection:
-        //   A MAC address, which we got above.
-        //   A Service ID or UUID.  In this case we are using the
-        //     UUID for SPP.
-        try {
-            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-        } catch (IOException e) {
-            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-        }
-
-        // Discovery is resource intensive.  Make sure it isn't going on
-        // when you attempt to connect and pass your message.
-        btAdapter.cancelDiscovery();
-
-        // Establish the connection.  This will block until it connects.
-        Log.d(TAG, "...Connecting to Remote...");
-        try {
-            btSocket.connect();
-            Log.d(TAG, "...Connection established and data link opened...");
-        } catch (IOException e) {
-            try {
-                btSocket.close();
-            } catch (IOException e2) {
-                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
-            }
-        }
-
-        // Create a data stream so we can talk to server.
-        Log.d(TAG, "...Creating Socket...");
-
-        try {
-            outStream = btSocket.getOutputStream();
-        } catch (IOException e) {
-            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        Log.d(TAG, "...In onPause()...");
-
-        if (outStream != null) {
-            try {
-                outStream.flush();
-            } catch (IOException e) {
-                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
-            }
-        }
-
-        try     {
-            btSocket.close();
-        } catch (IOException e2) {
-            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-        }
-    }
-
-    private void checkBTState() {
-        // Check for Bluetooth support and then check to make sure it is turned on
-
-        // Emulator doesn't support Bluetooth and will return null
-        if(btAdapter==null) {
-            errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
-        } else {
-            if (btAdapter.isEnabled()) {
-                Log.d(TAG, "...Bluetooth is enabled...");
-            } else {
-                //Prompt user to turn on Bluetooth
-                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-        }
-    }
-
     private void errorExit(String title, String message){
         Toast msg = Toast.makeText(getBaseContext(),
                 title + " - " + message, Toast.LENGTH_SHORT);
@@ -246,20 +161,4 @@ public class MainScreen extends ActionBarActivity {
         finish();
     }
 
-    private void sendData(String message) {
-        byte[] msgBuffer = message.getBytes();
-
-        Log.d(TAG, "...Sending data: " + message + "...");
-
-        try {
-            outStream.write(msgBuffer);
-        } catch (IOException e) {
-            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
-            if (address.equals("00:00:00:00:00:00"))
-                msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
-            msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
-
-            errorExit("Fatal Error", msg);
-        }
-    }
 }
